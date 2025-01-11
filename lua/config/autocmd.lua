c126ad2a6cf47fd09d9a88979e488b24c52519c5
Alex Valle <alexvalle75@gmail.com>
2025-01-11 13:34:51 +0100

    added foldcolumn to autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         require("ufo").detach()
         vim.opt_local.foldenable = false
-        vim.wo.foldcolumn = "0"
+        vim.opt_local.foldcolumn = '0'
     end
 })
