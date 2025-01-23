39129d922d1e1802fc4c6bb238dea33eb1db1e2f
Alex Valle <alexvalle75@gmail.com>
2025-01-23 23:37:45 +0100

    Added small thing

--- init.lua
+++ init.lua
@@ -25,3 +25,4 @@ require "config.autocmd"
 vim.cmd("highlight FoldColumn guibg=NONE guifg=NONE")
 vim.cmd("highlight SignColumn guibg=NONE guifg=NONE")
 vim.cmd("highlight CursorLineNr guibg=NONE guifg=NONE")
+-- vim.cmd("highlight LineNr guifg=#5eacd3 guibg=NONE") -- Line number color
