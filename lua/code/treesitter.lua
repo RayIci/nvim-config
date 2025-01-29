44974162b752e55f69fba655c1f0d30a811c8dfc
Alex Valle <alexvalle75@gmail.com>
2025-01-29 22:52:37 +0100

    Small treesitter changes

--- lua/code/treesitter.lua
+++ lua/code/treesitter.lua
@@ -30,7 +30,10 @@ return {
                 "dockerfile",
             },
             sync_install = false,
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
             indent = { enable = true },
         })
     end,
