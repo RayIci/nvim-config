cacfe66f90db697c261660ad25a14e3c210a5aed
Alex Valle <alexvalle75@gmail.com>
2025-01-30 19:29:01 +0100

    Update lazygit

--- lua/git/lazygit.lua
+++ lua/git/lazygit.lua
@@ -14,4 +14,9 @@ local M = {
     },
 }
 
+function M.config()
+    vim.g.lazygit_floating_window_scaling_factor = 1                                -- scaling factor for floating window
+    vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' } -- customize lazygit popup window border characters
+end
+
 return M
