8bad11241b6f349986a98efcf117e16d9ef20d8a
Alex Valle <alexvalle75@gmail.com>
2025-02-21 21:10:41 +0100

    Added colorschemes

--- lua/themes/colorscheme.lua
+++ lua/themes/colorscheme.lua
@@ -6,9 +6,12 @@ local themes = {
     onedark = "onedark",
     vscode2 = "vscode2",
     rosepine = "rose-pine",
+    vimcode = "vim-code",
+    material = "material",
+    github = "github"
 }
 
-local selected_theme = themes.rosepine
+local selected_theme = themes.onedark
 
 local M = {
     vim.tbl_extend("force", require("themes.all." .. selected_theme), { priority = 1000, lazy = false })
