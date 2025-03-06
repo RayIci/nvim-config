1438a38783188f700cd3c4eb002ec7e6af14a7b1
Alex Valle <alexvalle75@gmail.com>
2025-03-06 19:32:41 +0100

    Changed colorscheme

--- lua/themes/colorscheme.lua
+++ lua/themes/colorscheme.lua
@@ -11,7 +11,7 @@ local themes = {
     github = "github"
 }
 
-local selected_theme = themes.github
+local selected_theme = themes.sonokai
 
 local M = {
     vim.tbl_extend("force", require("themes.all." .. selected_theme), { priority = 1000, lazy = false })
