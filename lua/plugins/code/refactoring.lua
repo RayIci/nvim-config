cdd602bfef3e7b61647c7606655a69fb257ca68d
Alex Valle <alexvalle75@gmail.com>
2025-03-16 19:43:57 +0100

    Changed refactoring keymaps

--- lua/plugins/code/refactoring.lua
+++ lua/plugins/code/refactoring.lua
@@ -8,33 +8,38 @@ local M = {
 }
 
 function M.config()
-    require('refactoring').setup({})
+    local map = require "utils.keymaps".map
 
-    require("telescope").load_extension("refactoring")
+    require('refactoring').setup({})
 
     require "which-key".add {
         { "<leader>r", group = "Refactoring" }
     }
 
-    vim.keymap.set(
-        { "n", "x" },
-        "<leader>rr",
-        function() require('telescope').extensions.refactoring.refactors() end,
-        { desc = "Select" }
-    )
+    map("x", "<leader>re", ":Refactor extract ")
+    map("x", "<leader>rf", ":Refactor extract_to_file ")
 
-    vim.keymap.set(
+    map("x", "<leader>rv", ":Refactor extract_var ")
+
+    map({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+    map("n", "<leader>rI", ":Refactor inline_func")
+
+    map("n", "<leader>rb", ":Refactor extract_block")
+    map("n", "<leader>rbf", ":Refactor extract_block_to_file")
+
+    map(
         "n",
         "<leader>rp",
         function() require('refactoring').debug.printf({ below = false }) end,
         { desc = "Debug printf" }
     )
 
-    vim.keymap.set({ "x", "n" }, "<leader>rv", function() require('refactoring').debug.print_var() end,
+    map({ "x", "n" }, "<leader>rv", function() require('refactoring').debug.print_var() end,
         { desc = "Debug print var" })
     -- Supports both visual and normal mode
 
-    vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end,
+    map("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end,
         { desc = "Debug clear debug" })
 end
 
