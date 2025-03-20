5fdb908385991011f87147b61f7dfc172a2d3919
Alex Valle <alexvalle75@gmail.com>
2025-03-20 20:01:18 +0100

    Fixed wrong copilot chat command

--- lua/plugins/general/copilot-chat.lua
+++ lua/plugins/general/copilot-chat.lua
@@ -15,7 +15,7 @@ function M.keymaps()
     require "which-key".add({ "<leader>C", group = "Copilot chat", mode = {"n", "v"} })
     map({"v", "n"}, "<leader>Cc", "<cmd>CopilotChatToggle<cr>", { desc = "Open chat toggle" })
     map({"v", "n"}, "<leader>Cs", "<cmd>CopilotChatStop<cr>", { desc = "Stop output" })
-    map({"v", "n"}, "<leader>Cr", "<cmd>CopilotChatRestart<cr>", { desc = "Restart output" })
+    map({"v", "n"}, "<leader>Cr", "<cmd>CopilotChatReset<cr>", { desc = "Reset output" })
     map({"v", "n"}, "<leader>Cp", "<cmd>CopilotChatPrompts<cr>", { desc = "Show prompts" })
 end
 
