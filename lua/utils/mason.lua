5bcc2bf9c2ef23781ba8b895ca22da8e547eb8cf
Alex Valle <alexvalle75@gmail.com>
2025-03-28 21:32:29 +0100

    Added bin mason path util

--- lua/utils/mason.lua
+++ lua/utils/mason.lua
@@ -4,4 +4,8 @@ function M.mason_data_path()
     return vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
 end
 
+function M.mason_bins_path()
+    return M.mason_data_path() .. "/bin/"
+end
+
 return M
