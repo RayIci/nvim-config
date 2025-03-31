c0c635951a869d3bd2dce19183fcd0fa79e61d0e
Alex Valle <alexvalle75@gmail.com>
2025-03-31 19:06:00 +0200

    Updated c# support

--- lua/plugins/code/lsp-settings/omnisharp.lua
+++ lua/plugins/code/lsp-settings/omnisharp.lua
@@ -1,5 +1,8 @@
 return {
-    cmd = { "dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+    -- cmd = { "dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+    cmd = { require("utils.mason").mason_bins_path() .. "omnisharp" },
+    enable_import_completion = true,
+    organize_imports_on_format = true,
     settings = {
         FormattingOptions = {
             EnableEditorConfigSupport = true,
@@ -9,7 +12,7 @@ return {
             LoadProjectsOnDemand = nil,
         },
         RoslynExtensionsOptions = {
-            EnableAnalyzersSupport = nil,
+            EnableAnalyzersSupport = true,
             EnableImportCompletion = true,
             AnalyzeOpenDocumentsOnly = nil,
         },
