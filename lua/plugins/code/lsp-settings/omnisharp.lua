5a82dc9134a0f9f739fadd5d68ed2daa37cb8343
Alex Valle <alexvalle75@gmail.com>
2025-03-31 22:28:38 +0200

    Update omnisharp

--- lua/plugins/code/lsp-settings/omnisharp.lua
+++ lua/plugins/code/lsp-settings/omnisharp.lua
@@ -1,8 +1,8 @@
 return {
     -- cmd = { "dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
     cmd = { require("utils.mason").mason_bins_path() .. "omnisharp" },
-    enable_import_completion = true,
-    organize_imports_on_format = true,
+    -- enable_import_completion = true,
+    -- organize_imports_on_format = true,
     settings = {
         FormattingOptions = {
             EnableEditorConfigSupport = true,
