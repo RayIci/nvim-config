4d91827113d8ae1a1c18b5e03bb05e2da40dd708
Alex Valle <alexvalle75@gmail.com>
2025-04-30 21:09:04 +0200

    Commented specter keymaps not working

--- lua/configs/plugins/general/nvim-specter.lua
+++ lua/configs/plugins/general/nvim-specter.lua
@@ -6,5 +6,6 @@ require("which-key").add({
 
 map("n", "<leader>fs", "<cmd>lua require('spectre').toggle()<cr>", { desc = "Specter toggle" })
 map("n", "<leader>fw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", { desc = "Specter search word" })
-map("v", "<leader>fw", "<esc><cmd>lua require('spectre').open_visual()<CR>", { desc = "Specter search selected word" })
+-- TODO: Should review since not working
+-- map("v", "<leader>fw", "<esc><cmd>lua require('spectre').open_visual()<CR>", { desc = "Specter search selected word" })
 map("n", "<leader>ff", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Specter search in file" })
