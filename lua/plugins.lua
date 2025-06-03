95fd2130224802575275a3d4ef743a106f53ab17
Alex Valle <alexvalle75@gmail.com>
2025-06-03 18:31:47 +0200

    Added smooth cursor plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -168,3 +168,18 @@ mnvim.plugins.install({
         require("pqf").setup()
     end,
 })
+
+-- SMOOTH CURSOR
+mnvim.plugins.install({
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        -- stiffness = 0.5,
+        -- trailing_stiffness = 0.49,
+
+        stiffness = 0.8, -- 0.6      [0, 1]
+        trailing_stiffness = 0.5, -- 0.4      [0, 1]
+        stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+        trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+        distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+})
