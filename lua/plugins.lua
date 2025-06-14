242d4efb7eb921e8b7deb596c9e69ce1afc605fa
Alex Valle <alexvalle75@gmail.com>
2025-06-14 18:22:32 +0200

    Added smart indentation plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -429,6 +429,22 @@ mnvim.plugins.install({
     end,
 })
 
+-- BETTER SMART INDENTATION
+mnvim.plugins.install({
+    "Darazaki/indent-o-matic",
+    configff = function()
+        require("indent-o-matic").setup({
+            -- The values indicated here are the defaults
+            -- Number of lines without indentation before giving up (use -1 for infinite)
+            max_lines = 2048,
+            -- Space indentations that should be detected
+            standard_widths = { 2, 4, 8 },
+            -- Skip multi-line comments and strings (more accurate detection but less performant)
+            skip_multiline = true,
+        })
+    end,
+})
+
 -- BACKUP PLUGINS ---------------------------------------------------------------------------------
 --
 -- AVANTE: AI code completion
