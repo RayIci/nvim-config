891518f60100e44e93a412768997cb654606d78a
Alex Valle <alexvalle75@gmail.com>
2025-06-17 21:10:55 +0200

    added themes

--- lua/theme.lua
+++ lua/theme.lua
@@ -27,4 +27,42 @@ mnvim.plugins.install({
     --     "olimorris/onedarkpro.nvim",
     --     name = "onedarkpro",
     -- },
+    {
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+    },
+    {
+        "lunarvim/darkplus.nvim",
+        name = "darkplus",
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        name = "cyberdream",
+        config = function()
+            require("cyberdream").setup({
+                theme = "dark",
+                transparent = true,
+            })
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        name = "nightfox",
+    },
+    {
+        "sainnhe/gruvbox-material",
+        name = "gruvbox-material",
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+        name = "monokai-pro",
+    },
+    {
+        "Mofiqul/dracula.nvim",
+        name = "dracula",
+    },
 })
